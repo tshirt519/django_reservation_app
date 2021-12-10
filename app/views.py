de61f40bbf59745c0390e17f1dc3195dd6d1a5e7
tshirt <tshirt519@gmail.com>
2021-12-10 13:42:49 +0900

    ✨ staffモデル実装とstoreモデルとの紐付け

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
+from .models import Store, Staff
 
-class IndexView(LoginRequiredMixin, TemplateView):
-    template_name = 'app/index.html'
+class StoreView(View):
+    def get(self, request, *args, **kwargs):
+        store_data = Store.objects.all()
+
+        return render(request, 'app/store.html', {
+            'store_data': store_data
+        })
+
+class StaffView(View):
+    def get(self, request, *args, **kwargs):
+        store_data = get_object_or_404(Store, id=self.kwargs['pk'])
+        staff_data = Staff.objects.filter(store=store_data).select_related('user')
+
+        return render(request, 'app/staff.html', {
+            'store_data': store_data,
+            'staff_data': staff_data,
+        })
